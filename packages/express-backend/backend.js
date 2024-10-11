624d2b73e115d59aaa76200efb34107a2aa92a3b
srishmaulik <srishmaulik2004@gmail.com>
2024-10-10 17:21:36 -0700

    made a find by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -49,6 +49,7 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -57,6 +58,19 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
